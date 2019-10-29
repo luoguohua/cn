14a60c339d7e8b9004ae3d12d7fc6f14737d5caf
luoguohua <LuoGuoHua_Xin@163.com>
2019-10-29 20:28:20 +0800

    initProject
    初始化项目

---
+++ src/test/java/com/luogh/cn/CnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luogh.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
